3699699b5ec4d75d555b33ec7b852022ad08e870
GishaTheDeveloper <spaceshhz@gmail.com>
2020-11-28 19:30:36 +0200

    Add CameraController.cs

---
+++ Zero-Collision/Assets/_Project/Scripts/Player/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Gisha.ZeroCollision.Player
+{
+    [RequireComponent(typeof(Camera))]
+    public class CameraController : MonoBehaviour
+    {
+        [SerializeField] private float followSpeed = default;
+        [SerializeField] private Transform targetToFollow = default;
+
+        Transform _transform;
+
+        private void Awake()
+        {
+            _transform = transform;
+        }
+
+        private void Update()
+        {
+            FollowTarget();
+        }
+
+        private void FollowTarget()
+        {
+            Vector3 oldPos = _transform.position;
+            Vector3 newPos = new Vector3(targetToFollow.position.x, targetToFollow.position.y, _transform.position.z);
+            _transform.position = Vector3.Lerp(oldPos, newPos, followSpeed * Time.deltaTime);
+        }
+    }
+}
