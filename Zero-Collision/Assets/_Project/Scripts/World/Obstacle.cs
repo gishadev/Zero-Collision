8ff19645173886739427cb7bd9d248e9b56434a2
GishaTheDeveloper <spaceshhz@gmail.com>
2020-11-29 13:25:13 +0200

    Update ObstacleGenerator.cs to spawn and despawn obstacles

--- Zero-Collision/Assets/_Project/Scripts/World/Obstacle.cs
+++ Zero-Collision/Assets/_Project/Scripts/World/Obstacle.cs
@@ -1,9 +1,35 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 namespace Gisha.ZeroCollision.World
 {
     public class Obstacle : MonoBehaviour
     {
+        Transform _transform;
+        Transform _camTransform;
 
+        private void Awake()
+        {
+            _transform = transform;
+            _camTransform = Camera.main.transform;
+        }
+
+        IEnumerator DespawnChecking()
+        {
+            yield return new WaitUntil(() => IsReadyToBeDeactivated());
+            gameObject.SetActive(false);
+        }
+
+        bool IsReadyToBeDeactivated() => (_camTransform.position - _transform.position).x > 15f;
+
+        private void OnEnable()
+        {
+            StartCoroutine(DespawnChecking());
+        }
+
+        private void OnDisable()
+        {
+            StopAllCoroutines();
+        }
     }
 }
--- Zero-Collision/Assets/_Project/Scripts/World/ObstacleGenerator.cs
+++ Zero-Collision/Assets/_Project/Scripts/World/ObstacleGenerator.cs
@@ -1,20 +1,51 @@
 ﻿using Gisha.Optimisation;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Gisha.ZeroCollision.World
 {
     public class ObstacleGenerator : MonoBehaviour
     {
+        [Header("Spawning")]
         [SerializeField] private GameObject obstaclePrefab = default;
 
-        private void Start()
+        [SerializeField] private float minDistToSpawn = default;
+        [SerializeField] private float maxDistToSpawn = default;
+
+        public Vector2 PositionOutOfCameraView 
+            => (Vector2)_cam.transform.position + Vector2.right * (_cam.orthographicSize * Screen.width / Screen.height + 1.25f);
+
+        List<GameObject> _obstaclesList = new List<GameObject>();
+        float _distToSpawn;
+
+        Transform _lastSpawnedTrans;
+        Camera _cam;
+
+        private void Awake()
         {
-            SpawnObstacle();
+            _cam = Camera.main;
         }
 
-        void SpawnObstacle()
+        private void LateUpdate()
         {
-            PoolManager.Instantiate(obstaclePrefab, Vector2.zero, Quaternion.identity);
+            if (IsReadyToSpawn())
+            {
+                SpawnObstacle(PositionOutOfCameraView);
+                _distToSpawn = Random.Range(minDistToSpawn, maxDistToSpawn);
+            }
+        }
+
+        bool IsReadyToSpawn()
+            => _lastSpawnedTrans == null || (_lastSpawnedTrans.position - _cam.transform.position).x < _distToSpawn;
+
+        void SpawnObstacle(Vector2 newPosition)
+        {
+            GameObject o = PoolManager.Instantiate(obstaclePrefab, newPosition, Quaternion.identity);
+
+            if (!_obstaclesList.Contains(o))
+                _obstaclesList.Add(o);
+
+            _lastSpawnedTrans = o.transform;
         }
     }
 }
